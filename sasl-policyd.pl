209730c6a71adf2182bd7b2cf158da80f76719fc
Jan Nidzwetzki <kn@achfrag.net>
2013-10-23 10:43:29 +0200

    Initial release

---
+++ sasl-policyd.pl
@@ -0,0 +1,348 @@
+#!/usr/bin/perl
+#
+# SASL Policy service for Postfix
+#
+# This service observes the postfix sasl 
+# logins. If a misuse of a account is detected,
+# no new mails from this account will be accepted.
+#
+# 
+# ## Installation ##
+#
+# Add the following line to your main.cf
+#
+# smtpd_recipient_restrictions = permit_mynetworks, 
+#           check_policy_service inet:127.0.0.1:25025, 
+#           ....
+# 
+# 
+# ## License ##
+#
+# Copyright 2013 Jan Kristof Nidzwetzki 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#####################################
+
+use warnings;
+use strict;
+
+use threads;
+
+use DBI;
+use DBIx::Connector;
+
+use POSIX qw(setsid);
+use Sys::Syslog qw(:DEFAULT setlogsock);
+use IO::Socket::UNIX;
+use IO::Select;
+
+our $NAME = "sasl-policyd";
+our $VERSION = "0.1 alpha";
+our $FOREGROUND = 0;
+our $TCP_PORT = 25025;
+our $DEBUG = 0;
+our $VERBOSE = 1;
+
+our $DATABASE = "/tmp/.sasl_db";
+
+our $POSTFIX_SASL_LIMIT_MESSAGE = "action=510 Your account is blocked due "
+    . "rate limits. Please contact your technical support.\n\n";
+
+our $POSTFIX_OK = "action=dunno\n\n";
+
+our $USER = "postfix";
+our $GROUP = "postfix";
+
+# Database cleanup
+our $CLEANUP_PERIOD = 2 * 24 * 60 * 60; # 2d
+
+# IPs per time period
+our $DIFFERNT_IPS = 10;
+
+# Time period for calculation
+our $TIME_PERIOD = 60 * 60; # 1h
+
+# Logging 
+our $syslog_socktype = 'unix';
+our $syslog_facility = "mail";
+our $syslog_options  = "pid";
+our $syslog_priority = "info";
+our $syslog_ident    = "postfix/$NAME";
+
+# Subs
+sub log_debug($);
+sub log_verbose($);
+sub log_info($);
+sub writelog($);
+sub handle_connection($);
+sub sqlite_table_exists($$);
+sub create_tables($);
+sub insert_login($$$);
+sub get_ips_for_login($$$);
+
+# Open Databse
+my $conn = DBIx::Connector -> new("dbi:SQLite:dbname=$DATABASE", "", "");
+
+# Open Logfiles
+setlogsock($syslog_socktype);
+openlog($syslog_ident, $syslog_options, $syslog_facility);
+
+log_info("$NAME started");
+
+# Install signal handlers
+$SIG{'TERM'} = sub {
+   writelog("Got TERM signal, exiting");
+   exit 0;
+};
+
+$SIG{'QUIT'} = sub {
+   writelog("Got QUIT signal, exiting");
+   exit 0;
+};
+
+# Init database
+if(! sqlite_table_exists($conn, "LOGIN")) {
+   log_info("Create new Database structure");
+   create_tables($conn);
+}
+
+# daemonize
+if(! $FOREGROUND) {
+   
+   defined(my $pid = fork()) or die "Can't fork: $!";
+   
+   # Parent process
+   if($pid) {
+       exit();
+   }
+
+   open(STDIN, "< /dev/null");
+   open(STDERR, "> /dev/null");
+   open(STDOUT, "> /dev/null");
+
+   setsid();
+
+   # Open pidfile
+   open(PIF, ">/var/run/sasl-policyd.pid") or die $!;
+
+   # Drop priviliges
+   my $uid = getpwnam($USER)  or die "User $USER doesn't exist!";
+   my $gid = getgrnam($GROUP) or die "Group $GROUP doesn't exist!";
+
+   $> = $uid;
+   $) = $gid;
+   
+   writelog("New daemon started");
+
+   # Write pid
+   print PIF $$;
+   close(PIF);
+}
+
+# Write pid file
+
+my $socket = IO::Socket::INET->new(    
+              Proto  => 'tcp',
+              LocalPort   => $TCP_PORT,
+              Listen      => 10,
+              Reuse       => 1);
+
+$socket->autoflush(1);
+
+# Accept connections, start a new thread for every connection
+while(1) {
+   my $client_socket = $socket -> accept();
+   async(\&handle_connection, $client_socket) -> detach; 
+}
+
+###
+# Handle connections
+###
+sub handle_connection($) {
+   my $client_socket = shift;
+   my $answer = $POSTFIX_OK;
+
+   log_verbose("Handle new connection");
+
+   # data
+   my $ip = "";
+   my $username = "";
+
+   # Read and parse data
+   CLIENT_DATA: while(my $client_data = <$client_socket>) {
+
+       log_debug("Got $client_data");
+
+       # All data read
+       if($client_data !~ /=/) {
+          last CLIENT_DATA;
+       }
+
+       if($client_data =~ m/.*sasl_username=(.*)/) {
+          $username = $1;
+       }
+
+       if($client_data =~ m/.*client_address=(.*)/) {
+          $ip = $1;
+       }
+   }
+
+   # SASL Username and IP known?
+   if($ip && $username) {
+
+      # Insert login into database
+      insert_login($conn, $username, $ip);
+
+      my $ips = get_ips_for_login($conn, $username, $TIME_PERIOD);
+   
+      # Missuse detected, block login
+      if($ips >= $DIFFERNT_IPS) {
+         log_info("Reject user $username from ip $ip - got logins from "
+          . "$ips different IPs in the last time period");
+
+         $answer = $POSTFIX_SASL_LIMIT_MESSAGE;
+      }
+   }
+
+   log_debug("Send $answer to postfix");
+
+   $client_socket -> send($answer);
+   $client_socket -> close();
+   
+   log_debug("Connection closed");
+}
+
+
+###
+# Does table exisits?
+###
+sub sqlite_table_exists($$) {
+   my $conn = shift;
+   my $table = shift;
+
+   my $dbh = $conn->dbh;
+   
+   my $sth = $dbh->prepare(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='$table'");
+
+   $sth->execute();
+   
+   my $found = 0;
+   if($sth -> fetch()) {
+      $found = 1;
+   }   
+   $sth->finish();
+   return $found;
+}
+
+###
+# Create Database structure
+###
+sub create_tables($) {
+   my $conn = shift;
+   
+   my $dbh = $conn->dbh;
+
+   my $sth = $dbh->prepare(
+      "CREATE table LOGIN (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, "
+      . "ip TEXT, logintime INTEGER)");
+
+    $sth->execute(); 
+    $sth->finish();
+}
+
+###
+# Insert new login into database
+###
+sub insert_login($$$) {
+   my $conn = shift;
+   my $username = shift;
+   my $ip = shift;
+
+   my $dbh = $conn->dbh;
+
+   my $timestamp = time();
+
+   my $sth = $dbh->prepare
+      ("INSERT INTO LOGIN (username, ip, logintime) values (?, ?, ?)");
+
+  $sth -> execute($username, $ip, $timestamp);
+  $sth -> finish();
+}
+
+### 
+# Calculate the number of different IPs for a given SASL user in
+# a given timeperiod
+###
+sub get_ips_for_login($$$) {
+   my $conn = shift;
+   my $username = shift;
+   my $time_period = shift;
+
+   my $dbh = $conn -> dbh;
+
+   my $timestamp = time() - $time_period;
+
+   my $sth = $dbh -> prepare
+      ("SELECT count(DISTINCT IP) from LOGIN where username = ? "
+      . "and logintime > ?");
+
+   $sth -> execute($username, $timestamp);
+   my $row = $sth->fetchrow_arrayref();
+   my $ips = $row->[0];
+   $sth -> finish();
+
+   unless($ips) {
+      $ips = 0;
+   }
+   
+
+   # Do some database cleanup
+   $timestamp = time() - $CLEANUP_PERIOD;
+   $sth = $dbh->prepare(
+      "DELETE from LOGIN where logintime < ?");
+   $sth -> execute($timestamp);
+   $sth -> finish();
+
+   return $ips;
+}
+
+###
+# Write info message to logfile
+###
+sub log_info($) {
+   writelog(shift);
+}
+
+###
+# Write verbose message to logfile
+###
+sub log_verbose($) {
+   writelog(shift) if $VERBOSE;
+}
+
+###
+# Write debug message to logfile
+###
+sub log_debug($) {
+   writelog(shift) if $DEBUG;
+}
+
+###
+# Write message to logfile
+###
+sub writelog($) {
+   my $message = shift;
+   syslog($syslog_priority, $message);
+}
